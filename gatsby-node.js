37efcb887788611ef5aaf6b0f030539678424ef9
Vacilando <tjfulopp@gmail.com>
2021-02-02 22:04:48 +0100

    Adding a reporter message

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,17 @@
-const path = require(`path`)
+const path = require(`path`);
 
-const wtf = require('wtf_wikipedia') // Syntax 'import wtf from "wtf_wikipedia"' would not work yet as this is run by node.js, see https://github.com/gatsbyjs/gatsby/issues/7810
-wtf.extend(
-  require('wtf-plugin-summary')
-)
-wtf.extend(
-  require('wtf-plugin-html')
-)
+const wtf = require('wtf_wikipedia'); // Syntax 'import wtf from "wtf_wikipedia"' would not work yet as this is run by node.js, see https://github.com/gatsbyjs/gatsby/issues/7810
+wtf.extend(require('wtf-plugin-summary'));
+wtf.extend(require('wtf-plugin-html'));
 
 const { WikipediaFetcherList } = require(path.resolve(
   `./src/components/gatsby-wikipedia-fetcher-list`
-))
+));
 //console.log('WikipediaFetcherList', WikipediaFetcherList())
 
 // https://www.gatsbyjs.com/docs/how-to/images-and-media/preprocessing-external-images/
-const { createRemoteFileNode } = require('gatsby-source-filesystem')
-const { array } = require('prop-types')
+const { createRemoteFileNode } = require('gatsby-source-filesystem');
+const { array } = require('prop-types');
 
 /**
  * Implement Gatsby's Node APIs in this file.
@@ -27,11 +23,20 @@ const { array } = require('prop-types')
 // https://www.gatsbyjs.com/docs/reference/config-files/actions/#createNode
 
 exports.sourceNodes = async (
-  { actions, createNodeId, createContentDigest, store, cache, getNodes },
-  pluginOptions,
+  {
+    actions,
+    createNodeId,
+    createContentDigest,
+    store,
+    cache,
+    getNodes,
+    reporter,
+  },
+  pluginOptions
 ) => {
-  const { createNode } = actions
+  const { createNode } = actions;
 
+  reporter.info(`Starting to fetch all data from Wikipedia`);
   /*
   console.log('hereeeeee')
   getNodes().forEach(node => {
@@ -48,7 +53,7 @@ exports.sourceNodes = async (
   //console.log(getNodes())
   */
 
-  var wikiUserAgentMail = pluginOptions.email
+  var wikiUserAgentMail = pluginOptions.email;
 
   // Here we need to supply the actual list of articles and languages. //////////////////////////////////////////////
   // wikiArticles should be an array of Wikipedia article titles (redirects are automatic) or full URLs.
@@ -56,28 +61,31 @@ exports.sourceNodes = async (
   // No need to swap spaces by underlines.
   // Languages that match the Wikipedia articles in wikiArticles. Empty string is a possibility, e.g. for the cases where the article is specified by its full URL.
   // Default language = none specified. wtf_wikipedia allows that for requests that are specific enough, like unique articles, full Wikipedia URLs, etc.
-  const wikiArticlesLanguages_initial = WikipediaFetcherList(getNodes)
+  const wikiArticlesLanguages_initial = WikipediaFetcherList(getNodes);
   //console.log('wikiArticlesLanguages_initial', wikiArticlesLanguages_initial)
 
   // Do not continue if there's no input data.
-  if (!Array.isArray(wikiArticlesLanguages_initial) || wikiArticlesLanguages_initial.length === 0) {
-    console.log('There are no Wikipedia articles to be fetched.')
-    return
+  if (
+    !Array.isArray(wikiArticlesLanguages_initial) ||
+    wikiArticlesLanguages_initial.length === 0
+  ) {
+    console.log('There are no Wikipedia articles to be fetched.');
+    return;
   }
-  
+
   // Deduplicate the array of objects -- see https://stackoverflow.com/questions/2218999/remove-duplicates-from-an-array-of-objects-in-javascript/36744732#36744732
-  const things = new Object()
-  things.thing = new Array()
-  things.thing = wikiArticlesLanguages_initial
+  const things = new Object();
+  things.thing = new Array();
+  things.thing = wikiArticlesLanguages_initial;
   const wikiArticlesLanguages = things.thing.filter((thing, index) => {
-    const _thing = JSON.stringify(thing)
+    const _thing = JSON.stringify(thing);
     return (
       index ===
-      things.thing.findIndex(obj => {
-        return JSON.stringify(obj) === _thing
+      things.thing.findIndex((obj) => {
+        return JSON.stringify(obj) === _thing;
       })
-    )
-  })
+    );
+  });
   //console.log('wikiArticlesLanguages', wikiArticlesLanguages)
 
   // https://www.gatsbyjs.com/docs/debugging-async-lifecycles/#use-promiseall-if-necessary
@@ -131,7 +139,9 @@ exports.sourceNodes = async (
   }
   */
 
-  console.log('Fetching ' + wikiArticlesLanguages.length + ' items from Wikipedia.')
+  console.log(
+    'Fetching ' + wikiArticlesLanguages.length + ' items from Wikipedia.'
+  );
 
   wikiArticlesLanguages.forEach(async (val, i) => {
     // Crucial to use "async" in forEach in order to be able to use "await" for createRemoteFileNode
@@ -145,17 +155,17 @@ exports.sourceNodes = async (
             'Api-User-Agent': wikiUserAgentMail,
           }
         )
-        .then(docList => {
+        .then((docList) => {
           //console.log('wikiArticles[i]', wikiArticles[i])
           //console.log('docList.title()', docList.title())
           //console.log('typeof docList[0]', typeof docList[0])
           if (typeof docList[0] === 'undefined') {
             // docList is normally an array of objects, but due to a quirk in wtf_wikipedia it becomes just an object if there's just 1 result of the fetch. So we need to make an array of it. (NB wtf_wikipedia also deduplicates so [ 'Cosmology', 'https://en.wikipedia.org/wiki/Cosmology'] returns 1 result.)
             // See issue Fetching an array does not return array if there's just one item #418 https://github.com/spencermountain/wtf_wikipedia/issues/418
-            docList = [docList]
+            docList = [docList];
           }
           //console.log('docList.title()', docList[0].title())
-          return docList.map(doc => {
+          return docList.map((doc) => {
             return {
               requestArticle: wikiArticlesLanguages[i].article, // We need to know what was requested so that we later match this result to the vorg article.
               requestLang: wikiArticlesLanguages[i].language, // We need to know what was requested so that we later match this result to the vorg article.
@@ -165,55 +175,56 @@ exports.sourceNodes = async (
               extract: doc.sections(0).text(), // See https://github.com/spencermountain/wtf_wikipedia/issues/413
               extractHTML: doc.sections(0).html({ images: false }), // See https://github.com/spencermountain/wtf_wikipedia/issues/413 // https://github.com/spencermountain/wtf_wikipedia/tree/master/plugins/html // https://github.com/spencermountain/wtf_wikipedia/issues/415
               firstImage: doc.image(0).url(), // the full-size wikimedia-hosted url // https://github.com/spencermountain/wtf_wikipedia#docimages
-            }
-          })
+            };
+          });
           //console.log('page inside', page);
         }),
-    ])
+    ]);
     //console.log('page outside', page)
 
-    page = page[0]
+    page = page[0];
 
     // Processing requestArticle.
-    var requestArticle = page.requestArticle
+    var requestArticle = page.requestArticle;
     //console.log('requestArticle', requestArticle)
 
     // Processing requestLang
-    var requestLang = page.requestLang
+    var requestLang = page.requestLang;
     //console.log('requestLang', requestLang)
 
     // Processing title
-    var title = page.title
+    var title = page.title;
     //console.log('title', title)
 
     // Processing URL
-    var url = page.url
+    var url = page.url;
     //console.log('url', url)
 
     // Processing summary
-    var summary = page.summary
+    var summary = page.summary;
     //console.log('summary', summary)
 
     // Processing extract
-    var extract = page.extract
+    var extract = page.extract;
     //console.log('extract', extract)
 
     // Processing extract (in HTML)
-    var extractHTML = page.extractHTML
+    var extractHTML = page.extractHTML;
     //console.log('extractHTML', extractHTML)
 
     // Processing firstImage
-    var firstImage = '' // Use empty string instead of false because the property must be created in nodeData.
+    var firstImage = ''; // Use empty string instead of false because the property must be created in nodeData.
     if (page.firstImage.match(/.(jpg|jpeg|png|gif)$/i)) {
       // valid formats: jpg, jpeg, png, webp
-      firstImage = page.firstImage
-      }
+      firstImage = page.firstImage;
+    }
     //console.log('firstImage', firstImage)
     // Create a local version of the remote image. See https://www.gatsbyjs.com/docs/how-to/images-and-media/preprocessing-external-images/
     // https://www.gatsbyjs.com/plugins/gatsby-source-filesystem/
 
-    var fileNode = false
-    if (firstImage !== '') { // Create an image node only if firstImage is an empty string.
+    var fileNode = false;
+    if (firstImage !== '') {
+      // Create an image node only if firstImage is an empty string.
       fileNode = await createRemoteFileNode({
         url: firstImage,
         //parentNodeId: nodeID,
@@ -221,7 +232,7 @@ exports.sourceNodes = async (
         createNodeId,
         cache,
         store,
-      })
+      });
     }
 
     // Custom data we want to store in the node.
@@ -235,7 +246,7 @@ exports.sourceNodes = async (
       extractHTML: extractHTML,
       firstImage: firstImage,
       //localFile: fileNode ? fileNode.id : '',
-    }
+    };
     //console.log('nodeData', nodeData)
 
     // Compulsory fields; see https://www.gatsbyjs.com/docs/reference/config-files/actions/#createNode
@@ -249,15 +260,15 @@ exports.sourceNodes = async (
         //content: JSON.stringify(myData), // Optional.
         contentDigest: createContentDigest(nodeData), // Compulsory.
       },
-    }
+    };
     //console.log('nodeMeta', nodeMeta)
 
     // Now create the node.
-    var node = Object.assign({}, nodeData, nodeMeta)
+    var node = Object.assign({}, nodeData, nodeMeta);
     // if the file was created, attach the new node to the parent node
     if (fileNode) {
-      node.localFile___NODE = fileNode.id
+      node.localFile___NODE = fileNode.id;
     }
-    createNode(node)
-  })
-}
+    createNode(node);
+  });
+};
